6c70942aa101c859a8d206c938ea13b3973d2b95
HYUN JUNG KIM <hg102289983@gmail.com>
2026-05-22 14:15:27 +0900

    chore: main 병합 전 누락된 수정사항 최종 반영

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
-<<<<<<< Updated upstream
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-=======
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
->>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
